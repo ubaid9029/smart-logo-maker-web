1d1d0bc4da71c198ffcbebae72936a8d1dc25999
Arham Sarwar <arhamsarwar786@gmail.com>
2026-04-22 13:19:54 +0500

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -119,8 +119,25 @@ export default function RootLayout({ children }) {
           crossorigin="anonymous"
           strategy="afterInteractive"
         />
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KMJ9XGMX');
+          `}
+        </Script>
       </head>
       <body className="font-sans antialiased">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KMJ9XGMX"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <Providers>
           <ClientPersistGate>
             <ConditionalLayout>
